4e9ef6854e8245a7d4276c5bcbf5957dd41ef972
SantosMaycon <mayconalves.santos14@gmail.com>
2023-02-10 00:12:26 -0200

    tree - drooped wood script

--- Assets/Scripts/Craft/Tree.cs
+++ Assets/Scripts/Craft/Tree.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Tree : MonoBehaviour {
   [SerializeField] private float health;
+  [SerializeField] private GameObject woodPrefab;
+  [SerializeField] private int totalWood;
   private Animator animator;
+  private bool isCut = false;
   // Start is called before the first frame update
   void Start() {
     animator = GetComponent<Animator>();
@@ -18,12 +21,16 @@ public class Tree : MonoBehaviour {
     animator.SetTrigger("cutting");
 
     if (health <= 0) {
+      isCut = true;
       animator.SetTrigger("cut");
+      for (int i = 0; i < totalWood; i++) {
+        Instantiate(woodPrefab, transform.position + new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0), Quaternion.identity);
+      }
     }
   }
 
   private void OnTriggerEnter2D(Collider2D other) {
-    if (other.CompareTag("Axe")) {
+    if (other.CompareTag("Axe") && !isCut) {
       OnHit();
     }
   }
